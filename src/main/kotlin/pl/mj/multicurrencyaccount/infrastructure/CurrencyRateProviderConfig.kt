af2be2e78c144d364a05f4bcd6f6a41348f602e1
Mateusz Jaszewski <mjasz123@gmail.com>
2022-09-11 18:45:16 +0200

    externalized nbp api configuration

--- src/main/kotlin/pl/mj/multicurrencyaccount/infrastructure/CurrencyRateProviderConfig.kt
+++ src/main/kotlin/pl/mj/multicurrencyaccount/infrastructure/CurrencyRateProviderConfig.kt
@@ -1,18 +1,29 @@
 package pl.mj.multicurrencyaccount.infrastructure
 
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.client.RestTemplate
+import java.time.Duration
 
 @Configuration
+@EnableConfigurationProperties(CurrencyRateProviderConfig.NbpApiProperties::class)
 class CurrencyRateProviderConfig {
 
     @Bean
-    fun nbpApiRestTemplate() : RestTemplate {
+    fun nbpApiRestTemplate(properties: NbpApiProperties) : RestTemplate {
         return RestTemplateBuilder()
-                .rootUri("https://api.nbp.pl/api/exchangerates/rates/c/")
+                .rootUri(properties.url)
+                .setConnectTimeout(properties.readTimeout)
+                .setReadTimeout(properties.readTimeout)
                 .build()
     }
+
+    @ConstructorBinding
+    @ConfigurationProperties(prefix = "nbp.api")
+    data class NbpApiProperties(val url: String, val connectTimeout: Duration, val readTimeout: Duration)
 
 }
